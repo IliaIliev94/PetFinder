e795e3c8b38c4a1da03f54dcea232de65fc723e3
Ilia Iliev <ilia_iliev94@abv.bg>
2021-08-01 13:48:41 +0300

    Updated index statistics visualization by adding an ajax call when making a request to show the statistics info.

---
+++ PetFinder/Controllers/Api/StatisticsApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetFinder.Data;
+using PetFinder.Models.Api.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetFinder.Controllers.Api
+{
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public StatisticsApiController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public StatisticsResponseModel GetStatistics()
+        {
+            var statisticsData = new StatisticsResponseModel
+            {
+                TotalPosts = context.SearchPosts.Count(),
+                FoundPets = context.SearchPosts.Where(searchPost => searchPost.IsFound).Count(),
+                LostPets = context.SearchPosts.Where(searchPost => !searchPost.IsFound).Count(),
+            };
+
+            return statisticsData;
+        }
+
+    }
+}
---
+++ PetFinder/Models/Api/Statistics/StatisticsResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetFinder.Models.Api.Statistics
+{
+    public class StatisticsResponseModel
+    {
+        public int TotalPosts { get; init; }
+
+        public int LostPets { get; init; }
+
+        public int FoundPets { get; init; }
+    }
+}
